67a978561287122f07c49575573acee00c9baaca
henry226 <henry5581984@gmail.com>
2018-10-24 16:58:19 -0400

    Car classes

---
+++ Abstract Factory Pattern/MicroCar.java
@@ -0,0 +1,12 @@
+
+public class MicroCar extends Car{
+	MicroCar(Location location){
+		super(CarType.MICRO, location);
+		construct();
+	}
+	
+	@Override
+	protected void construct() {
+		System.out.println("Connecting to Micro Car");
+	}
+}
